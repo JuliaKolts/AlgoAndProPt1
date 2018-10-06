411f4fe9feee38015c834adee8cb1a1d8d9357ef
JuliaKolts <43311435+JuliaKolts@users.noreply.github.com>
2018-10-06 21:53:54 +0300

    Added Lab2_1

---
+++ Lab2/Lab2_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n = 1;
+	float sum = 0, a = 2;	//starting from a2
+	
+	do {
+		a *= ((2*n + 2)*pow(n,n))/pow(n-1,n-1);
+		sum += a;
+		n++;
+	} while (a >= 0.0001);
+	
+	printf("Sum: %f\n", sum + 2);	//adding a1 (a1 = 2)
+	
+	return 0;
+}
