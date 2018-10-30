c91f6002ff7902b8bc7be20c0ced59df4e1e92d3
JuliaKolts <43311435+JuliaKolts@users.noreply.github.com>
2018-10-31 02:02:01 +0300

    Update Lab3_1.c

--- Lab3/Lab3_1.c
+++ Lab3/Lab3_1.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
+int main(void)
 {
-	float y = 0, x = 0.1, sn = 0, se = 0;
+	double y = 0, x = 0.1, sn = 0, se = 0;
 	int i = 1;
 
-	for(int n = 1; n <= 35; n++)
+	for(x = 0.1; x <= 0.81; x += 0.07)
 	{
-		sn += (pow(x,n)*cos(n*(M_PI/3))/n);
-
-		for(x = 0.1; x <= 0.8; x += 0.07)
+		for(int n = 1; n < 35; n++)
 		{
-			y = (-0.5)*log(1-(2*x*cos(M_PI/3)) + pow(x,2));
-			printf("X:%.2f ", x);	printf("SN:%f ", sn);
-
-			do {
-				se += (pow(x,i)*cos(i*(M_PI/3))/i);
-				i++;
-				printf("SE:%f ", se);
-			} while(se < 0.0001);
-			printf("Y:%f\n", y);
+			sn += (pow(x,n)*cos(n*3.14/3))/n;
 		}
+	
+		do {
+			se += (pow(x,i)*cos(i*3.14/3))/i;
+			i++;
+		} while(se < 0.0001);
+	
+		y = (-0.5)*log(1-(2*x*cos(3.14/3)) + pow(x,2));
+	
+		printf("X = %.2f    SN = %f    SE = %f    Y = %f\n", x, sn, se, y);		
+
 	}
-	return 0;
 }
+
+	
+		
+			
+			
+				
+	
