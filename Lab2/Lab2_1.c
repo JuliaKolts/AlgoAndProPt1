0281d352867905e3a4df4fbb2e2465fd84c8ec59
JuliaKolts <43311435+JuliaKolts@users.noreply.github.com>
2018-11-06 03:07:06 +0300

    Update Lab2_1.c

--- Lab2/Lab2_1.c
+++ Lab2/Lab2_1.c
@@ -4,13 +4,13 @@
 int main()
 {
 	int n = 1;
-	float sum = 0, a = 2;	//starting from a2
+	double sum = 0, a = 2;		//starting from a2
 	
 	do {
 		a *= ((2*n + 2)*pow(n,n))/pow(n-1,n-1);
 		sum += a;
 		n++;
-	} while (a >= 0.0001);
+	} while (a <= 0.0001);
 	
 	printf("Sum: %f\n", sum + 2);	//adding a1 (a1 = 2)
 	
